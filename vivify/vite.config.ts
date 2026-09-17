895c09877a91ca978c5a77f8cdabc59a2a8be4f6
wambuiiiii <githinjijoyjane@gmail.com>
2026-09-17 10:14:03 +0300

    latest changes

--- vivify/vite.config.ts
+++ vivify/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     tailwindcss(), // Fixes Error 1: Compiles your styles.css
     TanStackRouterVite(),
